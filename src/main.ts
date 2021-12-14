a8fb01e748ba406861e330fdcbe863d6f3021917
noook <neil.richter@golem.ai>
2021-12-14 22:35:32 +0100

    remove unused files

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,8 @@
 import { createApp } from 'vue';
 import 'virtual:windi.css';
-import { createPinia } from 'pinia';
 import router from './router';
 import App from './App.vue';
 
 createApp(App)
-  .use(createPinia())
   .use(router)
   .mount('#app');
--- src/store/example.ts
+++
@@ -1,9 +0,0 @@
-import { defineStore } from 'pinia';
-
-const useProjectStore = defineStore('project', {
-  state: () => ({}),
-  getters: {},
-  actions: {},
-});
-
-export default useProjectStore;
